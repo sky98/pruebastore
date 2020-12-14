8ba138a5921b12a9df0c1451b194fb2a26dc3640
sky98 <sehuanez@gmail.com>
2020-12-13 23:14:48 -0500

    Migration User OK

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('nit');
             $table->string('name');
+            $table->string('telephone');
+            $table->string('address');
+            $table->boolean('state');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
